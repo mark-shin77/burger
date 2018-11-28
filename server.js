08c1f0f1eb93372f39b2baf8cae5d7379853864a
Mark <contact.mshin@gmail.com>
2018-11-28 14:17:59 -0700

    almost connected

---
+++ server.js
@@ -0,0 +1,17 @@
+// dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride('_method'));
+app.engine('handlebars', exphbs ({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+var PORT = 8080;
+app.listen(PORT);
