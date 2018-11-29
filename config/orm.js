3647433513c014d37b9831d3ed2f81fff194b5af
Mark <contact.mshin@gmail.com>
2018-11-29 15:41:42 -0700

    add function working

--- config/orm.js
+++ config/orm.js
@@ -2,15 +2,22 @@
 var connection = require("../config/connection.js");
 
 var orm = {
-    all: function(tableInput, cb){
-        connection.query('SELECT * FROM ' + tableInput + ';', function(err, results){
+    all: function(tableName, cb){
+        connection.query('SELECT * FROM ' + tableName + ';', function(err, result){
             if (err) throw err;
-            cb(results);
+            cb(result);
         })
     },
 
-    update: function(tableInput, condition, cb){
-        connection.query('UPDATE ' + tableInput + ' SET devoured = true WHERE id =' + condition + ';', function(err, result){
+    update: function(tableName, condition, cb){
+        connection.query('UPDATE ' + tableName + ' SET devoured = true WHERE id =' + condition + ';', function(err, result){
+            if(err) throw err;
+            cb(result);
+        })
+    },
+
+    create: function(tableName, val, cb){
+        connection.query("INSERT INTO " + tableName + " (burger_name) VALUES ('" + val + "');", function(err, result){
             if(err) throw err;
             cb(result);
         })
--- models/burger.js
+++ models/burger.js
@@ -8,6 +8,9 @@ var burger = {
     },
     update: function(id, cb){
         orm.update('burgers', id, cb);
+    },
+    create: function(name, cb){
+        orm.create('burgers', name, cb);
     }
 }
 
