f2c5a224802606ce62d8ade6bb70512ab7e5208e
Mark <contact.mshin@gmail.com>
2018-11-30 20:58:47 -0700

    done commenting

--- controllers/app.js
+++ controllers/app.js
@@ -1,21 +1,27 @@
+// Dependencies
 var express = require('express');
 var router = express.Router();
 var burger = require('../models/burger.js')
 
+// Main Screen URL
 router.get('/', function(req, res){
     burger.all(function(data){
         // console.log(data);
+        // Rending "index.handlebars & burger data from db"
         res.render('index', {data});
     })
 })
 
+// Update URL
 router.put('/burgers/update', function(req, res){
     burger.update(req.body.burger_id, function(result){
         // console.log(result);
+        // Redirecting to main screen after the data has been updated
         res.redirect('/');
     })
 })
 
+// Add Burgers URL
 router.post('/burgers/create', function(req,res){
     burger.create(req.body.burger_name, function(result){
         // console.log(result);
@@ -23,4 +29,5 @@ router.post('/burgers/create', function(req,res){
     })
 })
 
+// Exporting Routes to server
 module.exports = router;
